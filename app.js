b825513fbcb6215c1d50f2a3887a2ac3bafff4d2
MentosGun <mentosgun185@gmail.com>
2018-06-25 11:44:53 +0200

    Safety on playerInput

--- app.js
+++ app.js
@@ -2,28 +2,40 @@ let app = {
   playerInput: null,
   playerButton: document.querySelector('#playerButton'),
   players: document.querySelector('.players'),
+  divPlayers: document.querySelector('.divPlayers'),
+  divMatches: document.querySelector('.divMatches'),
   init: function() {
     // Je cible l'input "players"
     app.playerInput = document.querySelector('#playerInput')
+    app.playerInput.onkeyup = function(event){
+      if (event.keyCode === 13) {
+        app.checkPlayerInput()
+      }
+    }
     app.playerButton.onclick = function() {
-      app.createPlayer()
+      app.checkPlayerInput()
     }
   },
-  createPlayer: function() {
+  checkPlayerInput: function() {
+    let inputValue = app.playerInput.value.replace(/[^a-zA-Z0-9]/g, '')
+    app.createPlayer(inputValue)
+  },
+  createPlayer: function(inputValue) {
     app.players = document.querySelector('.players')
     let player = document.createElement('div')
     player.className = 'onePlayer'
-    player.innerHTML = app.playerInput.value
+    player.innerHTML = inputValue
     app.players.appendChild(player)
     app.playerInput.value = ''
     app.playersNumberCheck()
   },
-
   playersNumberCheck: function() {
     let numberOfPlayer = document.querySelectorAll('.onePlayer')
-    /*if (numberOfPlayer.length >= 3) {
-      app.
-    }*/
+    if (numberOfPlayer.length >= 3) {
+      app.playerButton.onclick = null
+      app.divPlayers.style.display = "none"
+      app.divMatches.style.display = "block"
+    }
   }
 }
 
