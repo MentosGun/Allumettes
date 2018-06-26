220717e23d50981816e45489763dfb2ac73be017
MentosGun <mentosgun185@gmail.com>
2018-06-26 14:36:00 +0200

    Fonctionnal APP and Restart Button

--- app.js
+++ app.js
@@ -10,8 +10,37 @@ let app = {
   divMatches: document.querySelector('.divMatches'),
   game: document.querySelector('.game'),
   status: document.querySelector('#status'),
+  gameButton: null,
+  remainingMatches: null,
+  instantPlayer: null,
+  restartGame: function() {
+    app.playerInput.value = ""
+    if (app.matchesInput.value) {
+      app.matchesInput.value = ""
+    }
+    while (app.players.firstChild) {
+      app.players.removeChild(app.players.firstChild)
+    }
+    if (app.divPlayers.style.display == "none") {
+      app.divPlayers.style.display = "block"
+    }
+    if (app.gameButton != null) {
+      app.gameButton.remove()
+    }
+    if (app.divMatches.style.display != "none") {
+      app.divMatches.style.display = "none"
+    }
+    if (document.querySelector('#selectMatches') != null) {
+      document.querySelector('#selectMatches').remove()
+    }
+    app.playerNumber = 0
+    app.status.innerHTML = ""
+    app.init()
+  },
   init: function() {
-    // Je cible l'input "players"
+    document.querySelector('#restart').onclick = function() {
+      app.restartGame()
+    }
     app.playerInput = document.querySelector('#playerInput')
     app.playerInput.onkeyup = function(event){
       if (event.keyCode === 13) {
@@ -24,12 +53,14 @@ let app = {
   },
   checkPlayerInput: function() {
     let inputValue = app.playerInput.value.replace(/[^a-zA-Z0-9]/g, '')
-    if (inputValue.length > 15) {
-      app.errorLength.style.display = "block"
-      app.errorLength.innerHTML = 'Pseudo trop long. 15 caractères Maximum!'
-    }
-    else {
-      app.createPlayer(inputValue)
+    if (inputValue != '') {
+      if (inputValue.length > 15) {
+        app.errorLength.style.display = "block"
+        app.errorLength.innerHTML = 'Pseudo trop long. 15 caractères Maximum!'
+      }
+      else {
+        app.createPlayer(inputValue)
+      }
     }
   },
   createPlayer: function(inputValue) {
@@ -41,7 +72,6 @@ let app = {
     player.setAttribute("name", inputValue)
     player.innerHTML = app.playerNumber + " - " + inputValue
     app.players.appendChild(player)
-    // console.log(app.playerInput.value.substr(app.playerInput.value.length - 1));
     app.playerInput.value = ''
     app.playersNumberCheck()
   },
@@ -83,19 +113,17 @@ let app = {
     }
   },
   startGame: function() {
+    app.matchesInput.value = ""
     app.divMatches.style.display = "none"
     app.status.style.margin = "auto"
     app.status.style.color = "black"
     app.status.style.fontSize = "1.5em"
     app.status.innerHTML = app.matchesInput.value + " Allumettes"
-    let select = document.createElement('div')
-    select.className = "select"
     app.createRadioInputs()
   },
   createRadioInputs: function() {
     let selectMatches = document.createElement('div')
     selectMatches.id = "selectMatches"
-    selectMatches.innerHTML = '<span class="playing"></span>'
     let index = 1
     do {
       let radio = document.createElement('input')
@@ -104,6 +132,9 @@ let app = {
       radio.setAttribute("type", "radio")
       radio.setAttribute("name", "radio")
       radio.setAttribute("value", index)
+      if (index === 1) {
+        radio.setAttribute("checked", "checked")
+      }
       let label = document.createElement('label')
       label.setAttribute("for", "radio" + index)
       label.innerHTML = index
@@ -112,11 +143,63 @@ let app = {
       index++
     } while (index <= 3);
     app.game.appendChild(selectMatches)
+    let gameButton = document.createElement('button')
+    gameButton.className = "buttonValidate"
+    gameButton.id = "gameButton"
+    gameButton.setAttribute("name", "game")
+    app.game.appendChild(gameButton)
+    app.gameButton = document.querySelector('#gameButton')
+    app.remainingMatches = app.status.textContent.substr(0, 2)
+    app.gameLoops()
   },
   gameLoops: function() {
-    /*do {
-
-    } while (true);*/
+    app.changePlayer()
+    app.gameButton.onclick = function() {
+      app.oneLoop()
+    }
+  },
+  changePlayer: function() {
+    app.instantPlayer = document.querySelector('#player' + app.playerNumber).textContent.substr(4)
+    app.gameButton.innerHTML = app.instantPlayer
+  },
+  oneLoop: function() {
+    let recoveredValue = null
+    let children = document.querySelectorAll('.radio')
+    for (var i = 0; i < children.length; i++) {
+      if (children[i].checked) {
+        recoveredValue = i+1
+      }
+    }
+    app.remainingMatches -= recoveredValue
+    if (app.remainingMatches === 1) {
+      app.status.innerHTML = app.remainingMatches + ' Allumette'
+    }
+    else {
+      app.status.innerHTML = app.remainingMatches + ' Allumettes'
+    }
+    app.playerNumber++
+    app.checkPlayerNumber()
+  },
+  checkPlayerNumber: function() {
+    if (app.remainingMatches <= 0) {
+      app.endGame()
+    }
+    else if (app.playerNumber > 3) {
+      app.playerNumber = 1
+      app.changePlayer()
+    }
+    else {
+      app.changePlayer()
+    }
+  },
+  endGame: function() {
+    app.gameButton.remove()
+    document.querySelector('#selectMatches').remove()
+    app.playerNumber = 0
+    app.showWinner()
+  },
+  showWinner: function() {
+    app.status.innerHTML = "Loser : " + app.instantPlayer
   }
 }
 
